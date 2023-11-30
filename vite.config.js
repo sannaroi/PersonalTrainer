5d5a79216e6e00528b4ed8cca88bb912eb83af99
sannaroi <sanna.roine@hotmail.com>
2023-11-30 10:39:05 +0200

    Deployment uus yritys

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personaltrainer/', // Add this row and use your own repository name
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
 })
